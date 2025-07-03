fb76719099d33ac0ca98c5616f75968801d572db
BakerNet <hans@multimediallc.com>
2025-07-03 12:47:20 -0700

    v1.3.1

--- tools/cli/main.go
+++ tools/cli/main.go
@@ -50,7 +50,7 @@ func main() {
 	app := &cli.App{
 		Name:        "codeowners-cli",
 		Usage:       "CLI tool for working with .codeowners files",
-		Version:     "v1.3.1.dev",
+		Version:     "v1.3.1",
 		Description: "",
 		Commands: []*cli.Command{
 			{
